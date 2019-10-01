dbae7ab7904624a62995e6ab6a865028dceae5e4
Israel Alagbe <israelalagbe53@gmail.com>
2019-10-01 22:39:07 +0100

    Admin login form

--- lib/property/property-admin-login.dart
+++ lib/property/property-admin-login.dart
@@ -6,6 +6,12 @@ class PropertyAdminLogin extends StatefulWidget {
 }
 
 class _PropertyAdminLoginState extends State<PropertyAdminLogin> {
+  final usernameCtl = TextEditingController();
+  final passwordCtl = TextEditingController();
+  _login() {
+    print(usernameCtl.value.text);
+    print(passwordCtl.value.text);
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,38 +26,74 @@ class _PropertyAdminLoginState extends State<PropertyAdminLogin> {
         onTap: () {
           FocusScope.of(context).requestFocus(new FocusNode());
         },
-        child: Container(
-          color: Colors.white,
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: <Widget>[
-              Container(
-                padding: EdgeInsets.all(10),
-                child: Column(
-                  children: <Widget>[
-                    TextFormField(
-                      decoration: InputDecoration(
-                          icon: Icon(Icons.person_outline),
-                          hintText: "Username",
-                          hasFloatingPlaceholder: true),
-                    ),
-                    SizedBox(
-                      width: double.infinity,
-                      child: RaisedButton(
-                        color: Colors.red,
-                        child: Text(
-                          "Login",
-                          style: TextStyle(color: Colors.white),
-                        ),
-                        onPressed: () {},
+        child: LayoutBuilder(builder:
+            (BuildContext context, BoxConstraints viewportConstraints) {
+          //Contrained box is used here cos of the single child scrollview
+          return SingleChildScrollView(
+              child: ConstrainedBox(
+            constraints: BoxConstraints(
+              minHeight: viewportConstraints.maxHeight,
+            ),
+            child: Container(
+              color: Colors.white,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    height: 300,
+                    child: Center(
+                      child: Text(
+                        "LOGO",
+                        textDirection: TextDirection.ltr,
+                        style: TextStyle(
+                            fontSize: 50,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.red),
                       ),
-                    )
-                  ],
-                ),
-              )
-            ],
-          ),
-        ),
+                    ),
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(10),
+                    child: Column(
+                      children: <Widget>[
+                        TextFormField(
+                          decoration: InputDecoration(
+                            prefixIcon: Icon(Icons.person_outline),
+                            labelText: "Username",
+                          ),
+                          controller: usernameCtl,
+
+                        ),
+                        TextFormField(
+                          decoration: InputDecoration(
+                            prefixIcon: Icon(Icons.lock),
+                            labelText: "Password",
+                          ),
+                          controller: passwordCtl,
+                          obscureText: true,
+                        ),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        SizedBox(
+                          width: 300,
+                          child: RaisedButton(
+                            color: Colors.red,
+                            child: Text(
+                              "Login",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                            onPressed: _login,
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ));
+        }),
       ),
     );
   }
